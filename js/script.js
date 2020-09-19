7947d33268eca302819604de107414a8f569d7c9
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-09-19 07:24:50 -0300

    Formulario

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function abrir(id){ 
+    document.getElementById(id).style.display = 'block'
+    
+}
+function fechar(id){
+    document.getElementById(id).style.display = 'none'
+}
+function envia(id){
+    document.getElementById(id).style.display = 'block'
+}
+function env(){
+   var campo = document.getElementsByTagName('input')
+    
+    
+}
