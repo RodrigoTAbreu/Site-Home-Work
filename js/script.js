a6a9a3eccfd9bc0e06778599a08af2ae85de3896
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-09-24 04:15:27 -0300

    Ajuste de Imagem e Form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-function abrir(id){ 
-    document.getElementById(id).style.display = 'block'
-    
-}
-function fechar(id){
-    document.getElementById(id).style.display = 'none'
-}
-function envia(id){
-    document.getElementById(id).style.display = 'block'
-}
-function env(){
-   var campo = document.getElementsByTagName('input')
-    
-    
-}
+document.getElementById('confirm').onreset = function envia(){
+    return confirm("Gostaria de resetar todos os campos?");  
+  };
