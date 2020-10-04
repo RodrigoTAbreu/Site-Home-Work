109fcd9d8551df336146ec327afe6003d7edcc0b
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-10-04 07:33:04 -0300

    Fomulario de contato.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-document.getElementById('confirm').onreset = function envia(){
-    return confirm("Gostaria de resetar todos os campos?");  
-  };
+/*document.getElementById('confirm').onreset = function envia(){
+  return confirm("Gostaria de resetar todos os campos?");  
+};*/
+
+
+function envia(){
+  let nome = document.getElementById('nome')
+  let name = Text(nome.value);
+  
+
+  let msg = document.getElementById('msg')
+
+
+  msg.innerHTML = ` Obrigado ${name} , confira agora sua caixa de email em , confirme seu cadastro e entraremos em contato. `;
+
+
+}
+
+
